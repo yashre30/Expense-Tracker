228f5eb0541e39628b6d5d69c7d3ad6cb806252f
yashre30 <kavisujatakavi@gmail.com>
2024-03-31 14:39:39 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import HomeComponent from "./modules/home";
+/*
+font-family: 'Jost', sans-serif;
+
+font-family: 'Montserrat', sans-serif;
+
+font-family: 'Poppins', sans-serif;
+
+font-family: 'Roboto', sans-serif;
+
+font-family: 'Spline Sans Mono', monospace;
+*/
+const Container = styled.div`
+  display: flex;
+  background-color: #e5e4e2;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  margin: 30px auto;
+  max-width: 1000px;
+  max-height: 1000px;
+`;
+
+const Header = styled.span`
+  color: #2a3439;
+  font-size: 25px;
+  font-weight: bolder;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>Expense Tracker</Header>
+      <HomeComponent />
+    </Container>
   );
 }
 
